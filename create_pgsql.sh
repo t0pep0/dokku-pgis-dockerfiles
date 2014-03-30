7d72faf30732ee4901dd510e988ce3a25efa9b31
Fernando Mumbach <fermuch@cryptolab.net>
2014-03-30 05:25:55 -0300

    added script to generate postgis extension

---
+++ create_pgsql.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [[ $1 && $2 ]]; then
+  # $1 = DB_PASSWORD
+  # $2 = PORT  
+  echo -e $1 $2
+  psql -h 172.17.42.1 -p $2 -d db <<< "CREATE EXTENSION postgis;"
+  psql -h 172.17.42.1 -p $2 -d db <<< "CREATE EXTENSION postgis_topology;"
+  psql -h 172.17.42.1 -p $2 -d db <<< "CREATE EXTENSION fuzzystrmatch;"
+  psql -h 172.17.42.1 -p $2 -d db <<< "CREATE EXTENSION postgis_tiger_geocoder;"
+fi
