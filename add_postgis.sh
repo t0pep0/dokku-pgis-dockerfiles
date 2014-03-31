36f777e257cc6f422268bfc69dedced7ba2338ae
Fernando Mumbach <fermuch@cryptolab.net>
2014-03-31 20:03:49 -0300

    added password to psql calls

--- add_postgis.sh
+++ add_postgis.sh
@@ -3,9 +3,9 @@
 if [[ $1 && $2 ]]; then
   # $1 = DB_PASSWORD
   # $2 = PORT  
-  echo "${1} ${2}"
-  psql -h 172.17.42.1 -p "${2}" -d db <<< "CREATE EXTENSION postgis;"
-  psql -h 172.17.42.1 -p "${2}" -d db <<< "CREATE EXTENSION postgis_topology;"
-  psql -h 172.17.42.1 -p "${2}" -d db <<< "CREATE EXTENSION fuzzystrmatch;"
-  psql -h 172.17.42.1 -p "${2}" -d db <<< "CREATE EXTENSION postgis_tiger_geocoder;"
+  printf "Password: %s\nPORT: %s" "${1}" "${2}"
+  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis;"
+  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_topology;"
+  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION fuzzystrmatch;"
+  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_tiger_geocoder;"
 fi
