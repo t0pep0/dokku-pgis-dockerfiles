a209fa64a00502214945f6f0a9c506a2a072d2ca
Fernando Mumbach <fermuch@cryptolab.net>
2014-05-15 21:57:33 -0300

    bugfix typo

--- add_postgis.sh
+++ add_postgis.sh
@@ -3,7 +3,7 @@
 if [[ $1 && $2 ]]; then
   # $1 = DB_PASSWORD
   # $2 = PORT  
-  printf "Password: %s\nPORT: %s" "${1}" "${2}\n"
+  printf "Password: %s\nPORT: %s\n" "${1}" "${2}"
   PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis;"
   PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_topology;"
   PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION fuzzystrmatch;"
