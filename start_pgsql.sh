5da144086c5c1e72b64e62442e143cc6ce394fc7
t0pep0 <t0pep0.gentoo@gmail.com>
2014-07-29 12:44:28 +0400

    load postgis func and sprital_ref_sys

--- start_pgsql.sh
+++ start_pgsql.sh
@@ -6,7 +6,6 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     chown -R postgres:postgres /opt/postgresql
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'template0';"
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE EXTENSION postgis;"
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8';"
     POSTGIS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql`
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres db --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "$POSTGIS_SQL"
