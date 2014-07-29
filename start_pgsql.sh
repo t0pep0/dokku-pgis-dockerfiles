0502fe4b46ab765555bfe91c0814a79c8890416c
t0pep0 <t0pep0.gentoo@gmail.com>
2014-07-29 10:16:28 +0400

    fix mistake

--- start_pgsql.sh
+++ start_pgsql.sh
@@ -5,7 +5,7 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     cp -a /var/lib/postgresql/* /opt/postgresql/
     chown -R postgres:postgres /opt/postgresql
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH ENCODING=UTF8 LC_COLLATE=C.UTF-8, LC_TYPE=C.UTF-8;"
+    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH ENCODING=UTF8 LC_COLLATE=C.UTF-8 LC_TYPE=C.UTF-8;"
     touch /opt/postgresql/initialized
 fi
 su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres           -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf  -c listen_addresses=*"
