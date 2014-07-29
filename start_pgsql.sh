cb98699a133ccc003173a0247d5bad45e2357937
t0pep0 <t0pep0.gentoo@gmail.com>
2014-07-29 12:52:03 +0400

    load postgis func and sprital_ref_sys

--- start_pgsql.sh
+++ start_pgsql.sh
@@ -8,9 +8,9 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'template0';"
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8';"
     POSTGIS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql`
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres db --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "$POSTGIS_SQL"
+    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single db  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "$POSTGIS_SQL"
     SPIRITAL_REF_SYS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql`
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres db --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "$SPIRITAL_REF_SYS_SQL"
+    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single db  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "$SPIRITAL_REF_SYS_SQL"
     touch /opt/postgresql/initialized
 fi
 su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres           -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf  -c listen_addresses=*"
