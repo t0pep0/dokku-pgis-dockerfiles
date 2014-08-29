63c050b3848c1a5356d961d4479b5bc0a055fe96
t0pep0 <t0pep0.gentoo@gmail.com>
2014-08-29 15:32:29 +0400

    upd

--- add_postgis.sh
+++ add_postgis.sh
@@ -4,8 +4,8 @@ if [[ $1 && $2 ]]; then
   # $1 = DB_PASSWORD
   # $2 = PORT  
   printf "Password: %s\nPORT: %s\n" "${1}" "${2}"
-  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis;"
-  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_topology;"
-  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION fuzzystrmatch;"
-  PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_tiger_geocoder;"
+  #PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis;"
+  #PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_topology;"
+  #PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION fuzzystrmatch;"
+  #PGPASSWORD=$1 psql -h 172.17.42.1 -p "${2}" -d db -c"CREATE EXTENSION postgis_tiger_geocoder;"
 fi
--- start_pgsql.sh
+++ start_pgsql.sh
@@ -7,10 +7,10 @@ if [[ ! -f /opt/postgresql/initialized ]]; then
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE USER root WITH SUPERUSER PASSWORD '$1';"
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "update pg_database set encoding = pg_char_to_encoding('UTF8') where datname = 'template0';"
     su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db WITH TEMPLATE=template0 ENCODING='UTF8' LC_COLLATE='C.UTF-8' LC_CTYPE='C.UTF-8';"
-    POSTGIS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql`
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "\c db; "$POSTGIS_SQL
-    SPIRITAL_REF_SYS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql`
-    su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single db  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "\c db; " $SPIRITAL_REF_SYS_SQL
+    #POSTGIS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql`
+    #su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "\c db; "$POSTGIS_SQL
+    #SPIRITAL_REF_SYS_SQL=`cat /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql`
+    #su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single db  -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "\c db; " $SPIRITAL_REF_SYS_SQL
     touch /opt/postgresql/initialized
-fi
+  fi
 su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres           -D  /var/lib/postgresql/9.3/main  -c config_file=/etc/postgresql/9.3/main/postgresql.conf  -c listen_addresses=*"
